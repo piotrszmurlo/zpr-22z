3fa9c22cb43cfb2244377907b3dbb319f8591732
matikurcze00 <m.roszkowski00@gmail.com>
2022-12-09 18:14:18 +0100

    do testow

---
+++ zpr-cpp/main.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+// #include <emscripten.h>
+
+
+
+  int sizeX = 1000;
+  int sizeY = 800;
+  int pWidth = 30;
+  int pHeight = 150;
+  int bWidth = 15;
+  int bHeight = 15;
+  
+
+EMSCRIPTEN_KEEPALIVE
+int* calculateBall(int bXPos, int bYPos, int  bXVel, int bYVel, int pLeftXPos, int pLeftYPos, int pRightXPos, int pRightYPos)
+{
+  //did ball hit paddle
+  if (bXPos - bWidth <=pWidth)
+      {
+        if(bYPos+bHeight > pLeftYPos-pHeight && bYPos-bHeight < pLeftYPos)
+        {
+          bXVel = bXVel * -1;
+          bYVel = bYVel * -1;
+        }   
+      }
+
+  else if (bXPos>=pRightXPos)
+    {
+      if(bYPos + bHeight > pLeftYPos-pHeight && bYPos - bHeight < pLeftYPos)
+      {
+        bXVel = bXVel * -1;
+        bYVel = bYVel * -1;
+      }
+    } 
+  //did ball hit wall
+  else if(bYPos+bHeight>=sizeY || bYPos<=0)
+    {
+      bYVel=bYVel*-1; 
+    }
+
+  bXPos += bXVel;
+  bYPos += bYVel;
+  int returnArray[4];
+  returnArray[0]=bXPos;
+  returnArray[1]=bYPos;
+  returnArray[2]=bXVel;
+  returnArray[3]=bYVel;
+  return returnArray;
+}
