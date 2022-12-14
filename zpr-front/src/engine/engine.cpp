b38a90fae7bb4e945154bcb83fe70987a552e94f
mateusz.roszkowski <mateusz.roszkowski@asseco.pl>
2022-12-14 22:53:03 +0100

    lekka zmiana w engine.cpp

--- zpr-front/src/engine/engine.cpp
+++ zpr-front/src/engine/engine.cpp
@@ -15,45 +15,47 @@ extern "C" {
     int bWidth = 15;
     int bHeight = 15;
     int pXPosition = 10;
+
     if(std::end(ball)-std::begin(ball)!=4)
       {
         return result;
       }
+    
     int bXPosition = ball[0]; 
     int bYPosition = ball[1]; 
-    int bXVel = ball[2];
-    int bYVel = ball[3];
+    int bXVelocity = ball[2];
+    int bYVelocity = ball[3];
     
     //did ball hit paddle
     if (bXPosition - bWidth <=pWidth + pXPosition)
         {
           if(bYPosition+bHeight > pLeftYPosition-pHeight && bYPosition-bHeight < pLeftYPosition)
           {
-            bXVel = bXVel * -1;
-            bYVel = bYVel * -1;
+            bXVelocity = bXVelocity * -1;
+            bYVelocity = bYVelocity * -1;
           }   
         }
     else if (bXPosition + bWidth>=sizeX - pWidth - pXPosition)
       {
         if(bYPosition + bHeight > pLeftYPosition-pHeight && bYPosition - bHeight < pLeftYPosition)
         {
-          bXVel = bXVel * -1;
-          bYVel = bYVel * -1;
+          bXVelocity = bXVelocity * -1;
+          bYVelocity = bYVelocity * -1;
         }
       } 
     //did ball hit wall
     else if(bYPosition+bHeight>=sizeY || bYPosition<=0)
       {
-        bYVel=bYVel*-1; 
+        bYVelocity=bYVelocity*-1; 
       }
 
-    bXPosition += bXVel;
-    bYPosition += bYVel;
+    bXPosition += bXVelocity;
+    bYPosition += bYVelocity;
 
     result[0]=bXPosition;
     result[1]=bYPosition;
-    result[2]=bXVel;
-    result[3]=bYVel;
+    result[2]=bXVelocity;
+    result[3]=bYVelocity;
     return result;
   }
 }
